c81f34a63224564ddfca456980e1fdf4c9ec86b4
Kristijan Kocev <kristijankocev1234@gmail.com>
2023-10-10 17:59:37 +0200

    reversal

--- reverse.py
+++ reverse.py
@@ -1,17 +1,13 @@
 def shift_list_right_inplace(lst, shift):
     size = len(lst)
-    shift %= size  # Ensure that shift is within the range of the list size
+    shift %= size  
 
-    # Reverse the entire list
     lst.reverse()
 
-    # Reverse the first part up to the shift position
     lst[:shift] = reversed(lst[:shift])
 
-    # Reverse the second part from shift position to the end
     lst[shift:] = reversed(lst[shift:])
 
-# Example usage:
 original_list = [1, 2, 3, 4, 5, 6]
 shift_amount = 2
 
--- reverseArrayHelper.js
+++ reverseArrayHelper.js
@@ -5,13 +5,24 @@ function shiftN(array, n) {
       return array; // No need to shift if n is zero
     }
   
+
     reverseArray(array, 0, length - 1); // Reverse the entire array
     reverseArray(array, 0, n - 1);      // Reverse the first part
     reverseArray(array, n, length - 1); // Reverse the second part
   
     return array;
   }
+
   
+
+
+  // [1,2,3,4,5,6]
+  // [6,5,4,      3,2,1]
+  
+  
+  // [5,6,   4,3,2,1] 
+  // [5,6, 1,2,3,4]
+
   // Helper function to reverse a portion of the array in-place
   function reverseArray(array, start, end) {
     while (start < end) {
@@ -23,7 +34,6 @@ function shiftN(array, n) {
     }
   }
   
-  // Example usage:
   const originalArray = [1, 2, 3, 4, 5, 6];
   const shiftAmount = 2;
   
